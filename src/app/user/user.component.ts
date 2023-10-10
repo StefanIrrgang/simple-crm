fc97be908a0fb7138506f38dbcfd92cd041fadf9
Stefan Irrgang <122272798+StefanIrrgang@users.noreply.github.com>
2023-10-10 09:18:57 +0200

    firestore live database and update table

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from 'src/models/user.class';
-
+import { Firestore, doc, addDoc, getFirestore, collection, getDocs, query, where, QuerySnapshot, DocumentSnapshot, onSnapshot, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -11,19 +12,42 @@ import { User } from 'src/models/user.class';
 })
 export class UserComponent implements OnInit {
 
+  firestore: Firestore = inject(Firestore);
+
   user = new User();
-  
+  users!: Observable<User[]>; // Hinzugefügt für die Anzeige der Benutzer in der Komponente
+  allUsers: User[] = [];
 
-  constructor(public dialog: MatDialog) {
-
-  }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
-    
+    const firestore = getFirestore();
+
+    const q = query(collection(firestore, 'users'));
+
+    // Änderungen in Echtzeit abonnieren
+    this.users = collectionData(q, { idField: 'id' }) as Observable<User[]>;
+
+    onSnapshot(q, (querySnapshot) => {
+      console.log('Received changes from DB ', querySnapshot);
+
+      this.allUsers = [];
+
+      querySnapshot.forEach((doc) => {
+        const userData = doc.data() as User;
+        this.allUsers.push(userData);
+        // console.log('User data: ', userData);
+      });
+      console.log('All users:', this.allUsers);
+    });
   }
 
   openDialog() {
-    this.dialog.open(DialogAddUserComponent)
+    this.dialog.open(DialogAddUserComponent);
+  }
 
+  formatBirthDate(milliseconds: number): string {
+    const date = new Date(milliseconds);
+    return date.toLocaleDateString('en-US'); // Hier kannst du das Format nach Bedarf anpassen
   }
 }
