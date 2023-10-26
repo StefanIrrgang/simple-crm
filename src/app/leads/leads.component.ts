f72919c53a9068f0b3233d1832e6d2885301c5eb
Stefan Irrgang <122272798+StefanIrrgang@users.noreply.github.com>
2023-10-26 12:13:00 +0200

    update diagramm

--- src/app/leads/leads.component.ts
+++ src/app/leads/leads.component.ts
@@ -2,20 +2,34 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from '../firebase.service';
 import { Chart } from 'chart.js/auto';
+import { OnDestroy } from '@angular/core';
+import { ChangeDetectorRef } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-leads',
   templateUrl: './leads.component.html',
   styleUrls: ['./leads.component.scss'],
 })
-export class LeadsComponent implements OnInit {
+export class LeadsComponent implements OnInit, OnDestroy {
   userData: any[] = [];
   newAmount: number = 0;
+  newAmounts: { [userId: string]: number | null } = {};
+  showOverlay: boolean = false;
+  overlayMessage: string = '';
 
   // Chart-Objekt
   chart!: Chart;
 
-  constructor(private firebaseService: FirebaseService) {}
+  constructor(private firebaseService: FirebaseService, private cdr: ChangeDetectorRef) { }
+
+  ngOnDestroy(): void {
+    // Überprüfe, ob das Diagramm vorhanden ist, bevor zerstören.
+    if (this.chart) {
+      this.chart.destroy();
+    }
+  }
 
   ngOnInit(): void {
     this.firebaseService.getUsersWithSum().subscribe((users) => {
@@ -27,6 +41,10 @@ export class LeadsComponent implements OnInit {
   }
 
   createChart(): void {
+    if (this.chart) {
+      this.chart.destroy();
+    }
+
     const labels = this.userData.map((user) => `${user.firstName} ${user.lastName}`);
     const data = this.userData.map((user) => user.summe);
 
@@ -52,13 +70,52 @@ export class LeadsComponent implements OnInit {
         },
       },
     });
+
+    this.cdr.detectChanges();
   }
 
-  addAmount(user: any, amount: number): void {
-    // Hier kannst du die Logik für das Hinzufügen von Beträgen implementieren
-    // Beispiel: user.summe += amount;
+  addAmount(user: any): void {
+    // Holen des Betrags für den aktuellen Benutzer
+    const amount = this.newAmounts[user.id] || 0;
+
+    // Hier die Summe aktualisieren
+    const newSum = user.summe + amount;
 
     // Firebase aktualisieren
-    this.firebaseService.updateUserSum(user.id, user.summe + amount);
+    this.firebaseService.updateUserSum(user.id, newSum);
+
+    // Lokal die Summe aktualisieren, damit das Diagramm sofort reagiert
+    user.summe = newSum;
+
+    // Diagramm aktualisieren
+    this.updateChart();
+
+    // Betrag zurücksetzen
+    this.newAmounts[user.id] = null;
+
+    this.showOverlay = true;
+    this.overlayMessage = 'Chart wurde aktualisiert';
+
+    // Verstecke das Overlay nach 2 Sekunden
+    setTimeout(() => {
+      this.showOverlay = false;
+    }, 2000);
   }
+
+  updateChart(): void {
+    // Überprüfen, ob das Diagramm bereits erstellt wurde
+    if (this.chart) {
+      // Aktualisieren der Daten
+      const labels = this.userData.map((user) => `${user.firstName} ${user.lastName}`);
+      const data = this.userData.map((user) => user.summe);
+
+      // Aktualisieren des Diagramms
+      this.chart.data.labels = labels;
+      this.chart.data.datasets[0].data = data;
+
+      // Neuzeichnen des Diagramms
+      this.chart.update();
+    }
+  }
+
 }
